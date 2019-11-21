e0c1f530ff0a8d667745ff3e8131bcc0ce92658f
ravi <flavio.ravier@gmail.com>
2019-11-21 15:11:49 -0300

    Fiz passo 2 e parte do 3. Achei que 2 já era pra fazer o array rs

---
+++ semana4/aula3-classes/blog/script.js
@@ -0,0 +1,18 @@
+var arrayPost = []
+const valorTitulo = document.getElementById("titulo").value
+const valorAutor = document.getElementById("autor").value
+const valorConteudo = document.getElementById("conteudo").value
+
+
+class post {
+    constructor(titulo, autor, conteudo) {
+        this.titulo = titulo
+        this.auto = autor
+        this.conteudo = conteudo
+    }
+
+}
+
+function criarPost() {
+    arrayPost.push(new post(valorTitulo, valorAutor, valorConteudo))
+}
