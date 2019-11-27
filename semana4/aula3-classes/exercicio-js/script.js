83858e682dfb8f23eebd711649eecf1e71d8fd85
ravi <flavio.ravier@gmail.com>
2019-11-27 07:23:35 -0300

    commit de exercicios de js da semana4 aula3

---
+++ semana4/aula3-classes/exercicio-js/script.js
@@ -0,0 +1,19 @@
+/* Exercicio 1 */
+
+/* O código possui tres arrays:
+numeros, array1 e array2. numeros tem um monte de numero, array1 e array2 tá vazio no inicio.
+
+Ele faz um for of q vai ler cada numero dentro do array numeros, aí ele coloca a condição de par impar.
+Se for par o numero, ele adiciona no array1, se for impar, vc já sabe ;)
+
+O console terá três linhas.
+
+A primeira lerá a quantidade total de numeros que tinhamos dentro de numeros.
+A segunda lerá a quantidade de numeros pares e 
+A terceira lerá a quantidade de numeros impares.
+*/
+
+/* Exercicio 2 */
+
+/* O código possui 1 array e dois numeros:
+ numeros, numero1 e numero2. numeros tem um monte de numero, numero1 vale infinito e numero2 vale 0 no inicio.*/
